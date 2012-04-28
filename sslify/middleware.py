dcd4acaec2fcd5cc331a1a5d531dcc0dddfd1907
Randall Degges <rdegges@gmail.com>
2012-04-28 16:12:39 -0700

    Adding our custom SSLifyMiddleware.

---
+++ sslify/middleware.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+
+class SSLifyMiddleware(object):
+    """Force all requests to use HTTPs. If we get an HTTP request, we'll just
+    force a redirect to HTTPs.
+
+    .. note::
+        This will only take effect if ``settings.DEBUG`` is False.
+    """
+    def process_request(self, request):
+        if not any((settings.DEBUG, request.is_secure(), request.META.get('HTTP_X_FORWARDED_PROTO', '') == 'https')):
+            url = request.build_absolute_uri(request.get_full_path())
+            secure_url = url.replace('http://', 'https://')
+            return HttpResponseRedirect(secure_url)
