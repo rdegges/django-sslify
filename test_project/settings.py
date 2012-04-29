c246c3abc0b1d587eb32e7300005a308bbd06d9d
Randall Degges <rdegges@gmail.com>
2012-04-29 13:06:22 -0700

    Adding 'sslify' to INSTALLED_APPS for testing.
    You've gotta have it in INSTALLED_APPS to test it with Django. Figures.

--- test_project/settings.py
+++ test_project/settings.py
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'sslify',
 )
 
 # A sample logging configuration. The only tangible logging
