6a9d2ed851ea8f34beb1675e0412777d5c48b82c
Randall Degges <rdegges@gmail.com>
2012-04-29 13:46:22 -0700

    Making the https redirect permanent.

--- sslify/middleware.py
+++ sslify/middleware.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponsePermanentRedirect
 
 
 class SSLifyMiddleware(object):
@@ -13,4 +13,4 @@ class SSLifyMiddleware(object):
         if not any((settings.DEBUG, request.is_secure(), request.META.get('HTTP_X_FORWARDED_PROTO', '') == 'https')):
             url = request.build_absolute_uri(request.get_full_path())
             secure_url = url.replace('http://', 'https://')
-            return HttpResponseRedirect(secure_url)
+            return HttpResponsePermanentRedirect(secure_url)
