bd4d59a1efecb5cfc367ba01ad6bc816f8c7d50d
Randall Degges <r@rdegges.com>
2014-03-08 18:12:26 -0800

    Adding trove classifiers.
    Bam!

--- setup.py
+++ setup.py
@@ -27,6 +27,23 @@ setup(
     url = 'https://github.com/rdegges/django-sslify',
     keywords = 'django ssl https middleware',
     description = 'Force SSL on your Django site.',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     long_description = open(normpath(join(dirname(abspath(__file__)),
         'README.md'))).read()
 
