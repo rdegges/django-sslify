5efea224e82222e973236f7f5fa456d984abba44
Alex Rothberg <agrothberg@gmail.com>
2014-11-24 00:34:27 -0500

    Added tests for custom SSL port

--- sslify/tests.py
+++ sslify/tests.py
@@ -1,3 +1,10 @@
+try:
+    # Python 2.x
+    from urlparse import urlsplit
+except ImportError:
+    # Python 3.x
+    from urllib.parse import urlsplit
+
 from django.http import HttpResponsePermanentRedirect
 from django.test import TestCase
 from django.test.client import RequestFactory
@@ -19,6 +26,16 @@ class SSLifyMiddlware(TestCase):
 
         self.assertIsInstance(request, HttpResponsePermanentRedirect)
         self.assertTrue(request['Location'].startswith('https://'))
+        self.assertEqual(443, urlsplit(request['Location']).port)
+
+    def test_custom_ssl_port(self):
+        custom_port = 8443
+        with self.settings(SSL_PORT=custom_port):
+            request = self.factory.get('/woot/')
+            middleware = _SSLifyMiddleware()
+            request = middleware.process_request(request)
+
+            self.assertEqual(custom_port, urlsplit(request['Location']).port)
 
     def test_disable_for_tests(self):
         """If disabled, we get a 404"""
