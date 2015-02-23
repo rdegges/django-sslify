ab8c71106fd4ed04f289830e2b2e7493fd487af7
Mike Hibbert <hibbert.michael@gmail.com>
2015-02-23 10:14:44 +0000

    more tweaks for url shortening

--- sslify/middleware.py
+++ sslify/middleware.py
@@ -35,19 +35,6 @@ class SSLifyMiddleware(object):
             if should_disable(request):
                 return None
 
-        try:
-            url = request.build_absolute_uri(request.get_full_path())
-
-            shorten_to_root_domain = getattr(settings, 'SSLIFY_SHORTEN_TO_ROOT_DOMAIN', False)
-
-            if shorten_to_root_domain:
-                url = url.replace('www.', '')
-
-            return HttpResponsePermanentRedirect(url)
-
-        except Exception, e:
-                print e.message
-
         # If we get here, proceed as normal.
         if not request.is_secure():
             try:
@@ -58,7 +45,12 @@ class SSLifyMiddleware(object):
 
                 ssl_port = getattr(settings, 'SSLIFY_PORT', 443)
 
+                shorten_to_root_domain = getattr(settings, 'SSLIFY_SHORTEN_TO_ROOT_DOMAIN', False)
+
                 hostname = url_split.hostname
+
+                if shorten_to_root_domain:
+                    hostname = hostname.replace('www.', '')
 
                 url_secure_split = (scheme, "%s:%d" % (hostname or '', ssl_port)) + url_split[2:]
                 secure_url = urlunsplit(url_secure_split)
