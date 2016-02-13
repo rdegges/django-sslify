ecac9801c40538e914e97024bb76b90bd851d9c6
cloverchio <chrisloverchio@gmail.com>
2016-02-12 18:58:10 -0700

    Updated .travis.yml and fixed broad exceptions.

--- test_project/templates.py
+++ test_project/templates.py
@@ -1,17 +1,18 @@
-
+import logging
 try:
-    # Django == 1.9
+    # Django 1.9
     from django.template import TemplateDoesNotExist
-except:
+except Exception as e:
     # Django >= 1.8
     from django.template.base import TemplateDoesNotExist
-
+    logging.exception(e)
 try:
     # Django >= 1.8
     # https://docs.djangoproject.com/en/1.8/releases/1.8/#django-template-loader-baseloader
     from django.template.loaders.base import Loader as BaseLoader
-except:
+except Exception as e:
     from django.template.loader import BaseLoader
+    logging.exception(e)
 
 
 class TestLoader(BaseLoader):
@@ -19,6 +20,6 @@ class TestLoader(BaseLoader):
 
     def load_template_source(self, template_name, template_dirs=None):
         if template_name == '404.html':
-            return ('404 error', None)
+            return '404 error', None
         else:
             raise TemplateDoesNotExist(template_name)
