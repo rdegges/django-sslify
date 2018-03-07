a090308c5f87f03f348d45cb5f0824bc6c70e463
Manel Clos <manelclos@gmail.com>
2018-03-07 08:12:10 +0100

    django-2.0 fixes

--- test_project/urls.py
+++ test_project/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'test_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
